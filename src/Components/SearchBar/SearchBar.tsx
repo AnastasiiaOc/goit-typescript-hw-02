69a12343945b4059471afa191e36ef38794b9df8
Anastasiia Savinova Occhiminuti <anastesie@hotmail.com>
2025-04-25 21:27:03 +0200

    Update SearchBar.tsx

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -5,17 +5,27 @@ import { FC } from "react";
 
 
 type SearchBarProps = {
-  onSearch: (value: string) => void;
+  onSearch: (query: string) => void;
 };
 const SearchBar: FC<SearchBarProps> =({ onSearch }) => {
   function handleSubmit(evt: FormEvent<HTMLFormElement>) {
     evt.preventDefault();
     // const query = evt.target.elements.search.value.trim();
-    const query = evt.target.elements.search.value.trim();
-    query === ""
-      ? toast.error("Search query cannot be empty!")
-      : onSearch(query);
-    evt.target.reset();
+    const target = evt.target as HTMLFormElement;
+    const query = (
+      target.elements.namedItem("query") as HTMLInputElement
+    ).value.trim();
+
+    if (query.trim() === "") {
+      toast.error("Search query cannot be empty!");
+      return;
+    }
+    // query.trim() === "" ? toast.error("Search query cannot be empty!"): onSearch(query);
+    // evt.target.reset();
+    onSearch(query);
+    if (target instanceof HTMLFormElement) {
+      target.reset();
+    }
   }
 
   return (
